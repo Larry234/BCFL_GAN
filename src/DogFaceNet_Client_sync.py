7d2f821103837954b270784946ddfaf12962296e
Larry234 <lazybear17@gmail.com>
2022-05-29 14:40:16 +0800

    initialize stage clear

--- src/DogFaceNet_Client_sync.py
+++ src/DogFaceNet_Client_sync.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torchvision
+from torchvision import transforms
 
 import web3
 import json
@@ -9,17 +11,29 @@ import argparse
 
 from config import *
 from utils import *
+from models import *
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--creator", help="Group creator", action="store_true")
-    parser.add_argument("--group", "ID of federated learning group", type=int)
-    parser.add_argument("--registry", "number of registries", type=int)
-    parser.add_argument("--round", "number of training rounds", type=int)
+    parser.add_argument("--group", help="ID of federated learning group", type=int)
+    parser.add_argument("--registry", help="number of registries", type=int)
+    parser.add_argument("--rounds", help="number of training rounds", type=int, default=10)
+    parser.add_argument("--epochs", help="epochs in each round", type=int, default=5)
+    parser.add_argument("--lr", help="learning rate", type=float, default=1e-3)
+    parser.add_argument("--device", help="gpu device id")
+    parser.add_argument("--img-size", help="generate image size", type=int, default=64)
+    parser.add_argument("--laten-dim", help="dimension of noise", type=int, default=100)
+    parser.add_argument("--batch-size", type=int, default=256)
 
     args = parser.parse_args()
+    batch_size = args.batch_size
+    laten_dim = args.laten_dim
+    img_size = args.img_size
+    rounds = args.rounds
+    epochs = args.epochs
 
     # initialize stage
     # Configure web3
@@ -33,6 +47,32 @@ if __name__ == '__main__':
     training_filter = contract_ins.events.StartTraining.createFilter(fromBlock='latest')
     aggregate_filter = contract_ins.events.aggregater_selected.createFilter(fromBlock='latest')
     model_filter = contract_ins.events.allModel_uploaded.createFilter(fromBlock='latest')
+
+    device = torch.device("cuda:{}".format(args.device) if torch.cuda.is_available() else "cpu")
+
+    # load dataset
+    transform =transforms.Compose([
+        transforms.Resize(img_size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    train_ds = torchvision.datasets.ImageFolder(DATAROOT, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_ds)
+
+    # define model
+    generator = Generator(laten_dim=laten_dim)
+    discriminator = Discriminator()
+    generator.to(device)
+    discriminator.to(device)
+
+    # define criterion
+    criterion = nn.BCELoss()
+    criterion.to(device)
+
+    # setup optimizer
+    optimizerG = torch.optim.Adam(generator.parameters(), lr=args.lr, betas=(0.5, 0.999))
+    optimizerD = torch.optim.Adam(discriminator.parameters(), lr=args.lr, betas=(0.5, 0.999))
 
     # ==========================================================
     # Grouping stage
@@ -53,6 +93,10 @@ if __name__ == '__main__':
 
     # ============================================================
     # Training stage
+    for round in range(rounds):
+
+        for epoch in range(epochs):
+            noise = torch.rand(batch_size, laten_dim, 1, 1)
 
 
 
--- src/models.py
+++ src/models.py
@@ -2,34 +2,60 @@ import torch
 import torch.nn as nn
 
 
-class ConvTransBlock(nn.Module):
-    def __init__(self, in_channel, out_channel, kernel_size, stride):
-        pass
 class Generator(nn.Module):
     
-    def __init__(self, laten_dim, ngf=64):
+    def __init__(self, laten_dim, ngf=64, nc=3):
         super(Generator, self).__init__()
         self.laten_dim = laten_dim
         self.main = nn.Sequential(
-            nn.ConvTranspose2d(self.laten_dim, ngf * 16, 7, 1, 0, bias=False),
+            nn.ConvTranspose2d(self.laten_dim, ngf * 16, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 16),
             nn.ReLU(),
-            # state size. (ngf*16) x 7 x 7
+            # state size. (ngf*16) x 4 x 4
             nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(),
-            # state size. (ngf*8) x 14 x 14
-            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 8),
+            # state size. (ngf*8) x 8 x 8
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
             nn.ReLU(),
-            # state size. (ngf*8) x 28 x 28
-            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 8),
+            # state size. (ngf*4) x 16 x 16
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
             nn.ReLU(),
-            # state size. (ngf*8) x  x 28
-            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 8),
-            nn.ReLU(),
-            # state size. (ngf*8) x 14 x 14
-
+            # state size. (ngf*2) x 32 x 32
+            nn.ConvTranspose2d(ngf * 2, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
         )
+
+    def forward(self, x):
+        return self.main(x)
+
+class Discriminator(nn.Module):
+    def __init__(self, ndf=64, nc=3):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, 4, stride=2, padding=1, bias=False), 
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf , ndf * 2, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4 
+            nn.Conv2d(ndf * 32, nc, 4, stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+            # state size. 1
+        )
+    
+    def forward(self, x):
+        return self.main(x)
